3aa4f04b8e06680623964313a75bdca4fed6b5f2
Michael Woolweaver <m.woolweaver@icloud.com>
2020-10-14 02:38:07 -0500

    Update pacility-parse.py

--- pacility-parse.py
+++ pacility-parse.py
@@ -3,14 +3,10 @@ from requests import get
 import json
 import re
 
-def contains_word(s, w):
-    return (' ' + w + ' ') in (' ' + s + ' ')
+# The repos we don't count/need 
+defaultRepos = ["http://cydia.zodttd.com/repo/cydia", "http://apt.modmyi.com", "http://apt.thebigboss.org/repofiles/cydia", "https://apt.bingner.com", "http://apt.saurik.com", "http://apt.thebigboss.org", "https://apt.procurs.us", "https://repo.packix.com", "https://repo.dynastic.co", "https://repounclutter.coolstar.org", "https://repo.theodyssey.dev", "https://repo.chariz.com", "https://checkra.in/assets/mobilesubstrate", "https://repo.chimera.sh"]
 
-word = b'Origin:'
-
-defaultRepos = ["http://apt.modmyi.com", "http://apt.thebigboss.org/repofiles/cydia", "https://apt.bingner.com", "http://apt.saurik.com", "http://apt.thebigboss.org", "https://apt.procurs.us", "https://repo.packix.com", "https://repo.dynastic.co", "https://repounclutter.coolstar.org", "https://repo.theodyssey.dev", "https://repo.chariz.com", "https://checkra.in/assets/mobilesubstrate", "https://repo.chimera.sh"]
-
-reposWithIssues = ["https://repo.sciency.us", "https://wizage.github.io/repo/", "https://iluwqaa.github.io/", "https://booleanmagic.com/repo", "https://cydiamy.github.io/1.0.4", "https://chickenmatt5.github.io/repo", "https://iamjamieq.github.io/repo", "http://rcrepo.com", "https://bandarhl.github.io", "https://xninja.xyz/apt", "https://apt.xninja.xyz"]
+reposWithIssues = ["https://repo.sciency.us", "https://wizage.github.io/repo/", "https://iluwqaa.github.io/", "https://booleanmagic.com/repo", "https://cydiamy.github.io/1.0.4", "https://chickenmatt5.github.io/repo", "https://iamjamieq.github.io/repo", "http://rcrepo.com", "https://bandarhl.github.io", "https://xninja.xyz/apt", "https://apt.xninja.xyz", "https://coolstar.org/publicrepo"]
 
 # Where we source our repo list from
 getParcility = get("https://api.parcility.co/db/repos/small")
@@ -22,142 +18,230 @@ jsonIRU = getIRU.json()
 parcility = jsonParcility["data"]
 IRU = jsonIRU
 
-beforeCheck = []
+listBeforeCheck = []
+
+listIRU = []
+listIRUDUP = []
 
 for repoIRU in IRU:
-    bc1 = repoIRU["url"].rstrip('/')
-    beforeCheck.append(bc1.lstrip())
+    lbc1 = repoIRU["url"].rstrip('/')
+    lbc1strip = lbc1.lstrip()
+    if lbc1strip not in listBeforeCheck:
+        listBeforeCheck.append(lbc1strip)
+        listIRU.append(lbc1strip)
+    else:
+        listIRUDUP.append(lbc1strip)
 
+listIRU.sort()
+listBeforeCheck.sort()
+
+listParcility = []
+listParcilityDUP = []
 for repoParcility in parcility:
-    bc2 = repoParcility["url"].rstrip('/')
-    beforeCheck.append(bc2.lstrip())
+    lbc2 = repoParcility["url"].rstrip('/')
+    lbc2strip = lbc2.lstrip()
+    if lbc2strip not in listBeforeCheck:
+        listBeforeCheck.append(lbc2strip)
+        listParcility.append(lbc2strip)
+    else:
+        listParcilityDUP.append(lbc2strip)
 
-res = []
-notRES = []
-defaultIn = []
-numWithIssues = []
+listParcility.sort()
+listBeforeCheck.sort()
 
-beforeCheck.sort()
-for i in beforeCheck:
-    if i not in res:
+listAfterCheck = []
+listAfterCheckDUP = []
+defaultInLBC = []
+WithIssuesInLBC = []
+
+for i in listBeforeCheck:
+    if i not in listAfterCheck:
         if i not in defaultRepos:
             if i not in reposWithIssues:
-                res.append(i)
+                listAfterCheck.append(i)
             else:
-                numWithIssues.append(i)
+                WithIssuesInLBC.append(i)
         else:
-            defaultIn.append(i)
+            defaultInLBC.append(i)
     else:
-        notRES.append(i)
+        listAfterCheckDUP.append(i)
 
-res.sort(reverse=True)
-maxRepo = len(res) - 1
 noFind = []
-found = []
-releaseFiles = []
-while maxRepo >= 0:
 
-        checkPackurl = res[maxRepo] + "/Release"
-        try:
-            checkPack = get(checkPackurl, headers={"User-Agent":"Debian APT-HTTP/1.3 (2.1.10)"}, timeout=2)
+releaseFilesLAC = []
 
-        except requests.exceptions.ReadTimeout as err:
-            print ("\n")
-            print ("Not added.")
-            print (maxRepo, res[maxRepo])
-            print (err)
-            print ("\n")
-            noFind.append([res[maxRepo], err])
-            maxRepo -= 1
-            continue # start the while loop over and try the next repo.
+listAfterCheck.sort()
+for repo in listAfterCheck:
 
-        except requests.exceptions.ConnectionError as err:
-            print ("\n")
-            print ("Not added.")
-            print (maxRepo, res[maxRepo])
-            print (err)
-            print ("\n")
-            noFind.append([res[maxRepo], err])
-            maxRepo -= 1
-            continue # start the while loop over and try the next repo.
+    checkPackurl = repo + "/Release"
 
-        if checkPack.status_code == 200:
-            print (maxRepo, res[maxRepo], checkPack.status_code)
-            found.append([res[maxRepo], checkPack.content])
-            maxRepo -= 1
+    try:
+        checkPack = get(checkPackurl, headers={"User-Agent":"Debian APT-HTTP/1.3 (2.1.10)"}, timeout=2)
 
-        else:
-            print ("\n")
-            print ("Not added.")
-            print (maxRepo, res[maxRepo], checkPack.status_code)
-            print ("\n")
-            noFind.append([res[maxRepo], checkPack.status_code])
-            maxRepo -= 1
+    except requests.exceptions.ReadTimeout as err:
+        print ("\n")
+        print ("Not added.")
+        print ((listAfterCheck.index(repo)+1), repo)
+        print (err)
+        print ("\n")
+        noFind.append([repo, err])
+        continue # start the while loop over and try the next repo.
 
-checkDup = []
-notDup = []
-isDup = []
-notValid = []
+    except requests.exceptions.ConnectionError as err:
+        print ("\n")
+        print ("Not added.")
+        print ((listAfterCheck.index(repo)+1), repo)
+        print (err)
+        print ("\n")
+        noFind.append([repo, err])
+        continue # start the while loop over and try the next repo.
 
-for file in found:
-    if file[1] not in checkDup:
-        if word in file[1]:
-            checkDup.append(file[1])
-            notDup.append(file[0])
-        else:
-            notValid.append(file)
+    if checkPack.status_code == 200:
+        print ((listAfterCheck.index(repo)+1), repo, checkPack.status_code)
+        releaseFilesLAC.append([repo, checkPack.content])
+
     else:
-        isDup.append(file)
+        print ("\n")
+        print ("Not added.")
+        print ((listAfterCheck.index(repo)+1), repo, checkPack.status_code)
+        print ("\n")
+        noFind.append([repo, checkPack.status_code])
+
+notDupURL = []
+notDupRelease = []
+
+isDupURL = []
+isDupRelease = []
+
+notValidRelease = []
+
+word = b'Origin:'
+for file in releaseFilesLAC:
+    if file[1] not in notDupRelease:
+        if word in file[1]:
+            notDupURL.append(file[0])
+            notDupRelease.append(file[1])
+        else:
+            notValidRelease.append(file)
+    else:
+        isDupURL.append(file[0])
+        isDupRelease.append(file[1])
+
+wasGH = []
+oldGH = []
+
+wasGL = []
+oldGL = []
+
+wasAS = []
+oldAS = []
+
+wasHTTP = []
+oldHTTP = []
+
+notChanged = []
+
+for dupRelease in isDupRelease:
+
+    indexND = notDupRelease.index(dupRelease)
+    indexID = isDupRelease.index(dupRelease)
+
+    boolGH = False
+    boolGL = False
+    boolAS = False
+    boolHTTP = False
+
+    if bool(re.search(r'\b(\.github\.io)\b', notDupURL[indexND])):
+        
+        indexID = isDupRelease.index(dupRelease)
+        oldGH.append([notDupURL[indexND], isDupURL[indexID]])
+        notDupURL[indexND] = isDupURL[indexID]
+        wasGH.append(notDupURL[indexND])
+        boolGH = True
+    
+    if bool(re.search(r'\b(\.gitlab\.io)\b', notDupURL[indexND])):
+        
+        indexID = isDupRelease.index(dupRelease)
+        oldGL.append([notDupURL[indexND], isDupURL[indexID]])
+        notDupURL[indexND] = isDupURL[indexID]
+        wasGL.append(notDupURL[indexND])
+        boolGL = True
+
+    if bool(re.search(r'\b(\.appspot\.com)\b', notDupURL[indexND])):
+        
+        indexID = isDupRelease.index(dupRelease)
+        oldAS.append([notDupURL[indexND], isDupURL[indexID]])
+        notDupURL[indexND] = isDupURL[indexID]
+        wasAS.append(notDupURL[indexND])
+        boolAS = True
+        
+    if bool(re.search(r'(http\:\/\/)', notDupURL[indexND])):
+        
+        indexID = isDupRelease.index(dupRelease)
+        oldHTTP.append([notDupURL[indexND], isDupURL[indexID]])
+        notDupURL[indexND] = isDupURL[indexID]
+        wasHTTP.append(notDupURL[indexND])
+        boolHTTP = True
+
+    if (boolGH == False and boolHTTP == False and boolGL == False and boolAS == False):
+        notChanged.append([notDupURL[indexND], isDupURL[indexID]])
 
 print ("\n")
 print ("\n")
+print ("There are " + str(len(listBeforeCheck)) + " repos on Parcility.co & ios-repo-updates.com combined")
 print ("\n")
-print ("Found " + str(len(notRES)) + " duplicate URLs.")
-#for url in notRES:
-    # print (url)
-
+print ("Found " + str(len(defaultInLBC)) + " default repos but will not add to list")
+#for default in defaultInLBC:
+#    print (default)
 print ("\n")
-print ("Found " + str(len(isDup)) + " duplicate release files.")
-#for dup in isDup:
-   # print (dup[0])
-
-print ("\n")
-print ("Found " + str(len(notValid)) + " with invalid release files.")
-#for invalid in notValid:
-#    print (invalid[0])
-
+print ("Found " + str(len(WithIssuesInLBC)) + " repos that could cause issues.")
+#for withIssues in WithIssuesInLBC:
+#    print (withIssues)
 print ("\n")
 print ("Failed to find " + str(len(noFind)) + " repos.")
 #for notFound in noFind:
 #    print (notFound)
-
 print ("\n")
-print ("Found " + str(len(numWithIssues)) + " repos that could cause issues.")
-#for withIssues in numWithIssues:
-#    print (withIssues)
-
-print("\n")
-print ("Found " + str(len(defaultIn)) + " default repos found but will not add to list")
-#for default in defaultIn:
-#    print (default)
-
+print ("Found " + str(len(notValidRelease)) + " with invalid release files.")
+#for invalid in notValidRelease:
+#    print (invalid[0])
 print ("\n")
-print ("Found " + str(len(notDup)) + " valid repos to be added to list.")
-#for ND in notDup:
-#    print (ND)
-
+print ("Found " + str(len(isDupRelease)) + " duplicate release files.")
+#for url in isDupURL:
+#   print (uel)
 print ("\n")
-print ("The combined number of repos on Parcility.co & ios-repo-updates.com")
-print (len(beforeCheck))
+print ("Found " + str(len(oldGH)) + " old github URL's that were changed to custom url.")
+#for url in oldGH:
+#    print (url)
+print ("\n")
+print ("Found " + str(len(oldGL)) + " old gitlab URL's that were changed to custom url.")
+#for url in oldGL:
+#    print (url)
+print ("\n")
+print ("Found " + str(len(oldAS)) + " old appspot URL's that were changed to custom url.")
+#for url in oldAS:
+#    print (url)
+print ("\n")
+print ("Found " + str(len(oldHTTP)) + " old http URL's changed to https.")
+#for url in oldHTTP:
+#    print (url)
+print ("\n")
+print ("Found " + str(len(notChanged)) + " URL's that didn't change even tho the release file was the same.")
+#print ("if (github.io OR gitlab.io) url is on the right it shouldn't change")
+#for url in notChanged:
+#    print (url)
+print ("\n")
+print ("Found " + str(len(notDupURL)) + " valid repos to be added to list.")
+#for url in notDupURL:
+#    print (url)
 
-
-notDup.sort()
+notDupURL.sort()
 with open('just-urls-sources.list', 'w') as f:
-    for url in notDup:
+    for url in notDupURL:
         if url != None:
             f.write("%s\n" % url)
 
 with open('sources.list', 'w') as f:
-    for repo in notDup:
+    for repo in notDupURL:
         if repo != None:
             f.write("deb %s/ ./\n" % repo)
